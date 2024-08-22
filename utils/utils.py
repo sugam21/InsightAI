388e17e0361d4fc01a624b240f48a86f90309df5
Sugam Sharma <sugams342@gmail.com>
2024-08-22 14:11:44 +0530

    added seed function and visualization function

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,9 @@
 import json
 from typing import Dict
+import numpy as np
+import random
+import torch
+import matplotlib.pyplot as plt
 
 
 class Config:
@@ -14,3 +18,42 @@ class Config:
         with open(config_path, mode='r') as file:
             config: Dict[str, any] = json.load(file)
         return cls(config['data'], config['train'], config['model'])
+
+
+def seed_everything(seed: int = 42) -> None:
+    """
+    This function seeds everything as name suggests
+    PARAMS:
+    seed(int): number to seed to
+
+    RETURNS:
+    none
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def visualize_image(loader, num_batch_to_show: int = 2) -> None:
+    """TODO: docstring"""
+    counter: int = 1
+    for image_batch, label_batch in loader:
+        fig, axes = plt.subplots(6, 5, figsize=(25, 8))
+        # train_features, train_labels = next(iter(loader))
+        for i, ax in enumerate(axes.flatten()):
+            img = image_batch[i].permute(1, 2, 0)
+            label = label_batch[i].item()
+            ax.imshow(img)
+            ax.set_title(label)
+            ax.set_axis_off()
+            print(f"Feature batch shape: {image_batch.size()}")
+            print(f"Labels batch shape: {label_batch.size()}")
+        plt.tight_layout()
+        plt.show()
+        counter += 1
+        if counter == num_batch_to_show:
+            break
