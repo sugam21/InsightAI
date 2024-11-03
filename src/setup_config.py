4bde221765cf1fca7b9095668561dc6ddebda0cd
Sugam Sharma <sugams342@gmail.com>
2024-11-03 14:11:46 +0530

    changed the argument type from str to Path or str

--- src/setup_config.py
+++ src/setup_config.py
@@ -11,13 +11,13 @@ class Config:
         self.train: dict[str, any] = train
 
     @classmethod
-    def from_json(cls, config_path: str):
+    def from_json(cls, config_path: Path | str):
         with open(config_path, mode="r") as file:
             config: dict[str, any] = json.load(file)
         return cls(config["data"], config["train"])
 
     @classmethod
-    def from_toml(cls, config_path: str):
+    def from_toml(cls, config_path: Path | str):
         with open(config_path, mode="rb") as file:
             config: dict[str, any] = tomllib.load(file)
         return cls(config["data"], config["train"])
