bb64b1c4bab971bc3e01c1e7d1a754ce34a217c6
Sugam Sharma <sugams342@gmail.com>
2024-11-06 20:21:36 +0530

    added azure openai llm and embeddings instead of local

--- src/rag/rag.py
+++ src/rag/rag.py
@@ -1,15 +1,19 @@
+import os
 from pathlib import Path
 
 import chromadb
+import dotenv
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_chroma import Chroma
 from langchain_ollama import ChatOllama, OllamaEmbeddings
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 from loguru import logger
 
 from src import Config
 
 RAG_CONFIG_PATH: str = Path("rag_config.json").resolve()
+dotenv.load_dotenv()
 
 class_name_mapping_dict = {
     "c0": "Alienware alpha or Alienware steam machine",
@@ -54,13 +58,30 @@ class RagPipeline:
         self.embeddings = OllamaEmbeddings(
             model=self.config.train["model"]["embedding_model"],
         )
-        callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-        self.llm = ChatOllama(
-            model=self.config.train["model"]["llm_model"],
-            num_thread=8,
-            verbose=True,
-            num_gpu=1,
-            callbacks=callback_manager,
+        self.embeddings = AzureOpenAIEmbeddings(
+            api_key=os.getenv("AZURE_OPENAI_KEY"),
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            model="text-embedding-3-large",
+            api_version="2023-05-15",
+            dimensions=1024,
+        )
+        # callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+        # self.llm = ChatOllama(
+        #     model=self.config.train["model"]["llm_model"],
+        #     num_thread=8,
+        #     verbose=True,
+        #     num_gpu=1,
+        #     callbacks=callback_manager,
+        # )
+
+        self.llm = AzureChatOpenAI(
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            openai_api_version="2024-08-01-preview",
+            model="gpt-4",
+            openai_api_key=os.getenv("AZURE_OPENAI_KEY"),
+            openai_api_type="azure",
+            temperature=0.3,
+            streaming=True,
         )
 
     def _setup_vector_store(self):
@@ -76,11 +97,7 @@ class RagPipeline:
             embedding_function=self.embeddings,
         )
 
-    @staticmethod
-    def format_docs(docs):
-        "\n\n".join(doc.page_content for doc in docs)
-
-    def run(self, query: str, image_class: str):
+    def _get_retriever(self, image_class):
         retriever = self.vector_store.as_retriever(
             search_type="similarity",
             search_kwargs={
@@ -88,27 +105,42 @@ class RagPipeline:
                 "filter": {"category": image_class},
             },
         )
+        return retriever
+
+    @staticmethod
+    def format_docs(docs):
+        "\n\n".join(doc.page_content for doc in docs)
+
+    def run(self, query: str, image_class: str):
         logger.debug(f"Image class: {image_class}")
         logger.debug(f"User query: {query}")
 
-        retrived_query = retriever.invoke(query)
+        retrived_query = self._get_retriever(image_class=image_class).invoke(query)
+        # logger.debug(retrived_query)
         formatted_docs = "\n\n".join(doc.page_content for doc in retrived_query)
-        result = self.llm.invoke(
-            f"""You are an expert assistant for question answering tasks.
-         Use the following context information to answer the question. If there is something repeating
-        in the conetne do not repeat that into the response.
-         If the context does not contain answer, just say that you don't know.\n\n
+        prompt = f"""You are an expert assistant for question answering tasks.
+         Use the following context information to answer the question. Only answer once, and always limit your answers to the document content only.
+         Never make up answers. If you do not have the answer, state that the data is not contained in your knowledge base and stop your response.
          Question: {query}
          Context:{formatted_docs}"""
-        )
-        return result
+        logger.debug(f"Prompt Length: {len(prompt)}")
+        # result = self.llm.invoke(prompt)
+        for chunk in self.llm.stream(prompt):
+            yield chunk
+        # return result
 
 
 if __name__ == "__main__":
     rp = RagPipeline(rag_config_path=RAG_CONFIG_PATH)
-    query = "What is the dimension of this model ?"
+    query = "What is the ram of this model ?"
     image_class = (class_name_mapping_dict["c14"]).strip()
-    while True:
-        result = rp.run(query, image_class)
-        print(result.content)
-        break
+    # while True:
+    # result = rp.run(query, image_class)
+    # print(result.content)
+    for chunk in rp.run(query=query, image_class=image_class):
+        print(chunk.content, end="", flush=True)
+    # break
+    # prompt = "Tell me something about Azure."
+    # for chunk in rp.llm.stream(prompt):
+    #     print(chunk.content, end="", flush=True)
+    # print(rp.llm)
