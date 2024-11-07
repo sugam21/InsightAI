3b712a4bc7f73b87dd89514c39a3e437ee6284a6
Sugam Sharma <sugams342@gmail.com>
2024-11-07 11:19:04 +0530

    workaround for permission denied

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 if ! grep -q "deb http://ftp.debian.org/debian stable main" /etc/apt/sources.list; then
-  echo "deb http://ftp.debian.org/debian stable main" >> /etc/apt/sources.list
+  echo "deb http://ftp.debian.org/debian stable main" | sudo tee -a  /etc/apt/sources.list
 fi
 apt update -y && apt upgrade -y && apt install -y sqlite3
